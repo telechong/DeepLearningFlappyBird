6f36a3bad3c2dfd680bacb85c2ff2a4bbd19c088
Tiancong Zheng <ztiancong817@gmail.com>
2016-11-23 21:03:03 +0100

    Collect and show game scores

--- deep_q_network.py
+++ deep_q_network.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 from __future__ import print_function
 
+from argparse import ArgumentParser
+from collections import deque
+import logging
 import tensorflow as tf
 import cv2
 import sys
@@ -8,32 +11,69 @@ sys.path.append("game/")
 import wrapped_flappy_bird as game
 import random
 import numpy as np
-from collections import deque
 
-GAME = 'bird' # the name of the game being played for log files
-ACTIONS = 2 # number of valid actions
-GAMMA = 0.99 # decay rate of past observations
-OBSERVE = 100000. # timesteps to observe before training
-EXPLORE = 2000000. # frames over which to anneal epsilon
-FINAL_EPSILON = 0.0001 # final value of epsilon
-INITIAL_EPSILON = 0.0001 # starting value of epsilon
-REPLAY_MEMORY = 50000 # number of previous transitions to remember
-BATCH = 32 # size of minibatch
+GAME = 'bird'  # the name of the game being played for log files
+ACTIONS = 2  # number of valid actions
+GAMMA = 0.99  # decay rate of past observations
+OBSERVE = 100000.  # timesteps to observe before training
+EXPLORE = 2000000.  # frames over which to anneal epsilon
+FINAL_EPSILON = 0.0001  # final value of epsilon
+INITIAL_EPSILON = 0.0001  # starting value of epsilon
+REPLAY_MEMORY = 50000  # number of previous transitions to remember
+BATCH = 32  # size of minibatch
 FRAME_PER_ACTION = 1
 
+LOGGER = logging.getLogger(__name__)
+
+
+class Score(object):
+
+    def __init__(self, debug):
+        self._debug = debug
+        self._current = 0
+        self._highest = 0
+        self._attempts = 1
+
+    def increase_current(self, reward):
+        """Increase the the score of current game."""
+        self._current = self._current + reward
+        if self._highest < self._current:
+            self._highest = self._current
+
+    def reset_current(self):
+        """Reset the current score."""
+        self._current = 0
+        self._attempts += 1
+
+    def show(self):
+        if not self._debug:
+            # Clear screen
+            print(chr(27) + "[2J")
+            print("statistics".upper())
+            print("-------------")
+            print(self.__str__())
+        else:
+            LOGGER.debug("ATTEMPT: %s, CURRENT SCORE: %s, HIGH SCORE: %s",
+                         self._attempts, self._current, self._highest)
+
+    def __str__(self):
+        return ("Attempt\t\t: {} \nCurrent score\t: {} \nHighscore\t: {}".
+                format(self._attempts, self._current, self._highest))
+
+
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev = 0.01)
+    initial = tf.truncated_normal(shape, stddev=0.01)
     return tf.Variable(initial)
 
 def bias_variable(shape):
-    initial = tf.constant(0.01, shape = shape)
+    initial = tf.constant(0.01, shape=shape)
     return tf.Variable(initial)
 
 def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides = [1, stride, stride, 1], padding = "SAME")
+    return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding="SAME")
 
 def max_pool_2x2(x):
-    return tf.nn.max_pool(x, ksize = [1, 2, 2, 1], strides = [1, 2, 2, 1], padding = "SAME")
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding="SAME")
 
 def createNetwork():
     # network weights
@@ -60,12 +100,12 @@ def createNetwork():
     h_pool1 = max_pool_2x2(h_conv1)
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2, 2) + b_conv2)
-    #h_pool2 = max_pool_2x2(h_conv2)
+    # h_pool2 = max_pool_2x2(h_conv2)
 
     h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 1) + b_conv3)
-    #h_pool3 = max_pool_2x2(h_conv3)
+    # h_pool3 = max_pool_2x2(h_conv3)
 
-    #h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
+    # h_pool3_flat = tf.reshape(h_pool3, [-1, 256])
     h_conv3_flat = tf.reshape(h_conv3, [-1, 1600])
 
     h_fc1 = tf.nn.relu(tf.matmul(h_conv3_flat, W_fc1) + b_fc1)
@@ -75,7 +115,10 @@ def createNetwork():
 
     return s, readout, h_fc1
 
-def trainNetwork(s, readout, h_fc1, sess):
+
+def trainNetwork(s, readout, h_fc1, sess, debug):
+    # initial score to keep track of the games' result
+    score = Score(debug)
     # define the cost function
     a = tf.placeholder("float", [None, ACTIONS])
     y = tf.placeholder("float", [None])
@@ -98,7 +141,7 @@ def trainNetwork(s, readout, h_fc1, sess):
     do_nothing[0] = 1
     x_t, r_0, terminal = game_state.frame_step(do_nothing)
     x_t = cv2.cvtColor(cv2.resize(x_t, (80, 80)), cv2.COLOR_BGR2GRAY)
-    ret, x_t = cv2.threshold(x_t,1,255,cv2.THRESH_BINARY)
+    ret, x_t = cv2.threshold(x_t, 1, 255, cv2.THRESH_BINARY)
     s_t = np.stack((x_t, x_t, x_t, x_t), axis=2)
 
     # saving and loading networks
@@ -107,9 +150,9 @@ def trainNetwork(s, readout, h_fc1, sess):
     checkpoint = tf.train.get_checkpoint_state("saved_networks")
     if checkpoint and checkpoint.model_checkpoint_path:
         saver.restore(sess, checkpoint.model_checkpoint_path)
-        print("Successfully loaded:", checkpoint.model_checkpoint_path)
+        LOGGER.info("Successfully loaded:", checkpoint.model_checkpoint_path)
     else:
-        print("Could not find old network weights")
+        LOGGER.info("Could not find old network weights")
 
     # start training
     epsilon = INITIAL_EPSILON
@@ -121,14 +164,14 @@ def trainNetwork(s, readout, h_fc1, sess):
         action_index = 0
         if t % FRAME_PER_ACTION == 0:
             if random.random() <= epsilon:
-                print("----------Random Action----------")
+                LOGGER.info("----------Random Action----------")
                 action_index = random.randrange(ACTIONS)
                 a_t[random.randrange(ACTIONS)] = 1
             else:
                 action_index = np.argmax(readout_t)
                 a_t[action_index] = 1
         else:
-            a_t[0] = 1 # do nothing
+            a_t[0] = 1  # do nothing
 
         # scale down epsilon
         if epsilon > FINAL_EPSILON and t > OBSERVE:
@@ -139,7 +182,7 @@ def trainNetwork(s, readout, h_fc1, sess):
         x_t1 = cv2.cvtColor(cv2.resize(x_t1_colored, (80, 80)), cv2.COLOR_BGR2GRAY)
         ret, x_t1 = cv2.threshold(x_t1, 1, 255, cv2.THRESH_BINARY)
         x_t1 = np.reshape(x_t1, (80, 80, 1))
-        #s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
+        # s_t1 = np.append(x_t1, s_t[:,:,1:], axis = 2)
         s_t1 = np.append(x_t1, s_t[:, :, :3], axis=2)
 
         # store the transition in D
@@ -159,7 +202,7 @@ def trainNetwork(s, readout, h_fc1, sess):
             s_j1_batch = [d[3] for d in minibatch]
 
             y_batch = []
-            readout_j1_batch = readout.eval(feed_dict = {s : s_j1_batch})
+            readout_j1_batch = readout.eval(feed_dict={s : s_j1_batch})
             for i in range(0, len(minibatch)):
                 terminal = minibatch[i][4]
                 # if terminal, only equals reward
@@ -169,7 +212,7 @@ def trainNetwork(s, readout, h_fc1, sess):
                     y_batch.append(r_batch[i] + GAMMA * np.max(readout_j1_batch[i]))
 
             # perform gradient step
-            train_step.run(feed_dict = {
+            train_step.run(feed_dict={
                 y : y_batch,
                 a : a_batch,
                 s : s_j_batch}
@@ -181,7 +224,7 @@ def trainNetwork(s, readout, h_fc1, sess):
 
         # save progress every 10000 iterations
         if t % 10000 == 0:
-            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step = t)
+            saver.save(sess, 'saved_networks/' + GAME + '-dqn', global_step=t)
 
         # print info
         state = ""
@@ -192,24 +235,53 @@ def trainNetwork(s, readout, h_fc1, sess):
         else:
             state = "train"
 
-        print("TIMESTEP", t, "/ STATE", state, \
-            "/ EPSILON", epsilon, "/ ACTION", action_index, "/ REWARD", r_t, \
-            "/ Q_MAX %e" % np.max(readout_t))
+        show_score(score, terminal, r_t)
+        LOGGER.debug("TIMESTEP %s/ STATE %s/ EPSILON %s/ ACTION %s/ REWARD %s / Q_MAX %e",
+                     t, state, epsilon, action_index, r_t, np.max(readout_t))
         # write info to files
         '''
         if t % 10000 <= 100:
             a_file.write(",".join([str(x) for x in readout_t]) + '\n')
             h_file.write(",".join([str(x) for x in h_fc1.eval(feed_dict={s:[s_t]})[0]]) + '\n')
             cv2.imwrite("logs_tetris/frame" + str(t) + ".png", x_t1)
+
         '''
 
-def playGame():
+
+def show_score(score, terminal, reward):
+    if not terminal:
+        if reward == 1:
+            score.increase_current(reward)
+    else:
+        score.reset_current()
+    score.show()
+
+
+def playGame(debug):
     sess = tf.InteractiveSession()
     s, readout, h_fc1 = createNetwork()
-    trainNetwork(s, readout, h_fc1, sess)
+    trainNetwork(s, readout, h_fc1, sess, debug)
+
+
+def _parse_args():
+    parser = ArgumentParser("Flappy bird DQN")
+    parser.add_argument('-d', '--debug', action="store_true",
+                        help='verbose information')
+    args = parser.parse_args()
+    return args
+
+
+def _setup_logging(debug=False):
+    lvl = logging.DEBUG if debug else logging.INFO
+    LOGGER.setLevel(lvl)
+    logging.basicConfig(format="%(message)s")
+
 
 def main():
-    playGame()
+    args = _parse_args()
+    _setup_logging(args.debug)
+    playGame(args.debug)
+
 
 if __name__ == "__main__":
     main()
